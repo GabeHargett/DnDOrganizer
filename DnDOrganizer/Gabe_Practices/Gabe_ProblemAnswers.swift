1d90902676b8e01db8fda3f1ac3e3f154fc5504a
Tanner Rozier <bouldermandalasomeday@gmail.com>
2022-03-03 17:42:18 -0700

    Added questions and answers

---
+++ DnDOrganizer/Gabe_Practices/Gabe_ProblemAnswers.swift
@@ -0,0 +1,37 @@
+//
+//  Gabe_ProblemAnswers.swift
+//  DnDOrganizer
+//
+//  Created by Tanner Rozier on 3/3/22.
+//
+
+import Foundation
+
+//Problem 1 Answer
+//private class func getIndexOfFirst(desiredValue: Int, numbers: [Int]) -> [Int] {
+//    var answer = [Int]()
+//    for (index, number) in numbers.enumerated() {
+//        if number == desiredValue {
+//            answer.append(index)
+//        }
+//    }
+//    return answer
+//}
+
+
+
+
+
+
+
+
+//Problem 2 Answer
+//private class func returnAllIntegersThatAreMultipleOf(value: Int, numbers: [Int]) -> [Int] {
+//    var answer = [Int]()
+//    for number in numbers {
+//        if number % value == 0 {
+//            answer.append(number)
+//        }
+//    }
+//    return answer
+//}
